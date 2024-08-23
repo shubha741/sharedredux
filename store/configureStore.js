ceab4ef8abb617dd11809df20895c935fb2ed478
shobhaAccion <shobha.v@accionlabs.com>
2024-08-23 19:33:01 +0530

    files added

--- store/configureStore.js
+++ store/configureStore.js
@@ -3,11 +3,16 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 import postReducer from '../reducers/postReducer';
 import commentReducer from '../reducers/commentReducer';
+import rootReducer from './reducers'; 
+import logger from 'redux-logger';
 
 const rootReducer = combineReducers({
   posts: postReducer,
   comments: commentReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
